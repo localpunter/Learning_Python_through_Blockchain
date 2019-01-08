39a72b11a431e6939c6c7265b49e626c191466df
localpunter <localpunter@sky.com>
2019-01-08 13:59:43 +0000

    code added to verify blockchain

--- blockchain.py
+++ blockchain.py
@@ -3,12 +3,17 @@ blockchain = []
 
 
 def get_last_blockchain_value():
-    """ Returns the last value of the current blockchain """
+    """ Returns the last value of the current blockchain.
+    If no value exists it returns None """
+    if len(blockchain) < 1:
+        return None
     return blockchain[-1]
 
 
-def add_value(transaction, last_transaction=[1]):
-    """ Appends a new valuse as well as the last value to the blockchain """
+def add_transaction(transaction, last_transaction=[1]):
+    """ Appends a new value as well as the last value to the blockchain """
+    if last_transaction == None:
+        last_transaction = [1]
     blockchain.append([last_transaction, transaction])
 
 
@@ -31,29 +36,47 @@ def print_blockchain_elements():
         print(block)
 
 
-# Get the first transaction input and add to the blockchain
-tx_amount = get_transaction_value()
-add_value(tx_amount)
+def verify_chain():
+    block_index = 0
+    is_valid = True
+    for block in blockchain:
+        print(block)
+        if block_index == 0:
+            block_index += 1
+            continue
+        elif block[0] == blockchain[block_index - 1]:
+            is_valid = True
+        else:
+            is_valid = False
+            break
+        block_index += 1
+    return is_valid
 
 
 while True:
     print('Please choose:')
     print('1: Add a new transaction value')
     print('2: Output the blockchain blocks')
+    print('h: Manipulate the chain')
     print('q: Quit')
     user_choice = get_user_choice()
-    if user_choice == '1':
+    if user_choice == "1":
         tx_amount = get_transaction_value()
-        add_value(tx_amount, get_last_blockchain_value())
-    elif user_choice == '2':
+        add_transaction(tx_amount, get_last_blockchain_value())
+    elif user_choice == "2":
         print_blockchain_elements()
-    elif user_choice == 'q':
+    elif user_choice == "h":
+        if len(blockchain) >= 1:
+            blockchain[0] = [2]
+    elif user_choice == "q":
         break
     else:
         print('Input was invalid, please pick a value from the list!')
+    if not verify_chain():
+        print("Invalid blockchain")
+        break
 
     print('Choice registered!')
-
 
 
 print('Done!')
--- test.py
+++ test.py
@@ -1,16 +1,29 @@
-name = input('what is your name? ')
+# name = input('what is your name? ')
 
-age = int(input('what is your age? '))
+# age = int(input('what is your age? '))
 
-def print_user_data():
-    print(name, age)
+# def print_user_data():
+#     print(name, age)
 
-#def print_values(arg1, arg2):
-    #print(arg1, arg2) 
+# #def print_values(arg1, arg2):
+#     #print(arg1, arg2)
 
-def print_decades_i_lived():
-    return int(age / 10) 
+# def print_decades_i_lived():
+#     return int(age / 10)
 
-print_user_data()
-#print_values(45, 'abc')
-print(print_decades_i_lived())
+# print_user_data()
+# #print_values(45, 'abc')
+# print(print_decades_i_lived())
+
+
+name = "Alan"
+age = 45
+
+if name == "Rachel" and age > 40 or age < 40:
+    print("Yes")
+
+if name == "Rachel" and (age > 30 or age < 40):
+    print("Hi Rachel!")
+else:
+    print("You're not Rachel!")
+
