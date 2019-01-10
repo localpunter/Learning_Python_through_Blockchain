a9f8355170a261a5911c7c3a64173d079f549b48
localpunter <localpunter@sky.com>
2019-01-10 12:38:34 +0000

    Section 3 - Working with Loops and Conditionals - complete

--- blockchain.py
+++ blockchain.py
@@ -43,25 +43,37 @@ def print_blockchain_elements():
     for block in blockchain:
         print('Outputting Block')
         print(block)
+    else:
+        print("-" * 50)
 
 
 def verify_chain():
-    block_index = 0
+    # block_index = 0
     is_valid = True
-    for block in blockchain:
+    for block_index in range(len(blockchain)):
         if block_index == 0:
-            block_index += 1
             continue
-        elif block[0] == blockchain[block_index - 1]:
+        elif blockchain[block_index][0] == blockchain[block_index - 1]:
             is_valid = True
         else:
             is_valid = False
             break
-        block_index += 1
+    # for block in blockchain:
+    #     if block_index == 0:
+    #         block_index += 1
+    #         continue
+    #     elif block[0] == blockchain[block_index - 1]:
+    #         is_valid = True
+    #     else:
+    #         is_valid = False
+    #         break
+    #     block_index += 1
     return is_valid
 
 
-while True:
+waiting_for_input = True
+
+while waiting_for_input:
     print('Please choose:')
     print('1: Add a new transaction value')
     print('2: Output the blockchain blocks')
@@ -77,14 +89,17 @@ while True:
         if len(blockchain) >= 1:
             blockchain[0] = [2]
     elif user_choice == "q":
-        break
+        waiting_for_input = False
     else:
         print('Input was invalid, please pick a value from the list!')
     if not verify_chain():
+        print_blockchain_elements()
         print("Invalid blockchain")
         break
+else:
+    print("User left")
 
-    print('Choice registered!')
+
 
 
 print('Done!')
--- test.py
+++ test.py
@@ -16,14 +16,32 @@
 # print(print_decades_i_lived())
 
 
-name = "Alan"
-age = 45
+# name = "Alan"
+# age = 45
 
-if name == "Rachel" and age > 40 or age < 40:
-    print("Yes")
+# if name == "Rachel" and age > 40 or age < 40:
+#     print("Yes")
 
-if name == "Rachel" and (age > 30 or age < 40):
-    print("Hi Rachel!")
-else:
-    print("You're not Rachel!")
+# if name == "Rachel" and (age > 30 or age < 40):
+#     print("Hi Rachel!")
+# else:
+#     print("You're not Rachel!")
+
+""" with 1 argument we print out from 0 up to the number argument(5)
+i.e. 0,1,2,3,4 in this case """
+# for i in range(5):
+#     print(i)
+
+""" with 2 arguments we print out from the first argument(5) up to the second argument(10)
+i.e. 5,6,7,8,9 in this case """
+# for i in range(5, 10):
+#     print(i)
+
+""" with 3 arguments we print out from the first argument(0) up to the second argument(40)
+in increments of the third argument(5) i.e. 0,5,10,15,20,25,30,35 in this case
+range ONLY WORKS with integers, + or - but NOT floats """
+for i in range(0, 40, 5):
+    print(i)
+
+
 
