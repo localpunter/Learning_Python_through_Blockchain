612e3751ab128fce19b73df1827da1e7a014f197
localpunter <localpunter@sky.com>
2019-01-18 14:59:18 +0000

    Adding File Access to our Blockchain

--- blockchain.py
+++ blockchain.py
@@ -24,6 +24,24 @@ owner = "Alan"
 participants = {"Alan"}
 
 
+def load_data():
+    with open("blockchain.txt", mode="r") as f:
+        file_content = f.readlines()
+        global blockchain
+        global open_transactions
+        blockchain = file_content[0]
+        open_transactions = file_content[1]
+
+load_data()
+
+
+def save_data():
+    with open("blockchain.txt", mode="w") as f:
+        f.write(str(blockchain))
+        f.write("\n")
+        f.write(str(open_transactions))
+
+
 def valid_proof(transactions, last_hash, proof):
     guess = (str(transactions) + str(last_hash) + str(proof)).encode()
     guess_hash = hash_string_256(guess)
@@ -97,11 +115,13 @@ def add_transaction(recipient, sender=owner, amount=1.0):
     #     "recipient": recipient,
     #     "amount": amount
     # }
-    transaction = OrderedDict([("sender", sender), ("recipient", recipient), ("amount", amount)])
+    transaction = OrderedDict(
+        [("sender", sender), ("recipient", recipient), ("amount", amount)])
     if  verify_transaction(transaction):
         open_transactions.append(transaction)
         participants.add(sender)
         participants.add(recipient)
+        save_data()
         return True
     return False
 
@@ -119,7 +139,8 @@ def mine_block():
     #     "recipient": owner,
     #     "amount": MINING_REWARD
     # }
-    reward_transaction = OrderedDict([("sender", "MINING"), ("recipient", owner), ("amount", MINING_REWARD)])
+    reward_transaction = OrderedDict(
+        [("sender", "MINING"), ("recipient", owner), ("amount", MINING_REWARD)])
     # Copy transaction instead of manipulating the original open_transactions list
     # This ensures that if for some reason the mining should fail, we don't have the reward transaction stored in the open transactions
     copied_transactions = open_transactions[:]
@@ -131,6 +152,7 @@ def mine_block():
         "proof": proof
     }
     blockchain.append(block)
+    save_data()
     return True
 
 
