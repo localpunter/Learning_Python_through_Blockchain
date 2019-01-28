05ece7d9c107db05ca0b969dd4fe03f0a6005b97
localpunter <localpunter@sky.com>
2019-01-28 22:30:44 +0000

    Documents folder tidied up

---
+++ Documents/Section 10 - Modules/modules-04-saving-and-loading-keys/wallet.py
@@ -0,0 +1,40 @@
+from Crypto.PublicKey import RSA
+import Crypto.Random
+import binascii
+
+class Wallet:
+    def __init__(self):
+        self.private_key = None
+        self.public_key = None
+
+    def create_keys(self):
+        private_key, public_key = self.generate_keys()
+        self.private_key = private_key
+        self.public_key = public_key
+        
+
+    def save_keys(self):
+        if self.public_key != None and self.private_key != None:
+            try:
+                with open('wallet.txt', mode='w') as f:
+                    f.write(self.public_key)
+                    f.write('\n')
+                    f.write(self.private_key)
+            except (IOError, IndexError):
+                print('Saving wallet failed...')
+
+    def load_keys(self):
+        try:
+            with open('wallet.txt', mode='r') as f:
+                keys = f.readlines()
+                public_key = keys[0][:-1]
+                private_key = keys[1]
+                self.public_key = public_key
+                self.private_key = private_key
+        except (IOError, IndexError):
+            print('Loading wallet failed...')
+
+    def generate_keys(self):
+        private_key = RSA.generate(1024, Crypto.Random.new().read)
+        public_key = private_key.publickey()
+        return (binascii.hexlify(private_key.exportKey(format='DER')).decode('ascii'), binascii.hexlify(public_key.exportKey(format='DER')).decode('ascii'))
