61496216b82a380fa0ce381bde30ac97924f8043
localpunter <localpunter@sky.com>
2019-02-02 11:39:37 +0000

    Routes set up for managing the Wallet

--- node.py
+++ node.py
@@ -9,6 +9,38 @@ wallet = Wallet()
 blockchain = Blockchain(wallet.public_key)
 CORS(app)
 
+@app.route("/wallet", methods=["POST"])
+def create_keys():
+    wallet.create_keys()
+    if wallet.save_keys():
+        response = {
+            "public_key": wallet.public_key,
+            "private_key": wallet.private_key
+        }
+        global blockchain
+        blockchain = Blockchain(wallet.public_key)
+        return jsonify(response), 201
+    else:
+        response = {
+            "message": "Saving the keys failed!"
+        }
+        return jsonify(response), 500
+
+@app.route("/wallet", methods=["GET"])
+def load_keys():
+    if wallet.load_keys():
+        response = {
+            "public_key": wallet.public_key,
+            "private_key": wallet.private_key
+        }
+        global blockchain
+        blockchain = Blockchain(wallet.public_key)
+        return jsonify(response), 201
+    else:
+        response = {
+            "message": "Loading the keys failed!"
+        }
+        return jsonify(response), 500
 
 @app.route("/", methods=["GET"])
 def get_ui():
