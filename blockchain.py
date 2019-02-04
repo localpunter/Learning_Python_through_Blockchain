c1f42326c2f9fe6fed24cba09283d3940359ed0e
localpunter <localpunter@sky.com>
2019-02-04 14:05:10 +0000

    Sending Requests from Python and Adding the Function & Route to Broadcast Transactions

--- blockchain.py
+++ blockchain.py
@@ -3,6 +3,7 @@ import hashlib as hl
 
 import json
 import pickle
+import requests
 
 # Import two functions from our hash_util.py file. Omit the ".py" in the import
 from utility.hash_util import hash_block
@@ -115,12 +116,15 @@ class Blockchain:
             proof += 1
         return proof
 
-    def get_balance(self):
+    def get_balance(self, sender=None):
         """Calculate and return the balance for a participant.
         """
-        if self.public_key == None:
-            return None
-        participant = self.public_key
+        if sender == None:
+            if self.public_key == None:
+                return None
+            participant = self.public_key
+        else:
+            participant = sender
         # Fetch a list of all sent coin amounts for the given person (empty lists are returned if the person was NOT the sender)
         # This fetches sent amounts of transactions that were already included in blocks of the blockchain
         tx_sender = [[tx.amount for tx in block.transactions
@@ -152,7 +156,7 @@ class Blockchain:
     # One required one (transaction_amount) and one optional one (last_transaction)
     # The optional one is optional because it has a default value => [1]
 
-    def add_transaction(self, recipient, sender, signature, amount=1.0):
+    def add_transaction(self, recipient, sender, signature, amount=1.0, is_receiving=False):
         """ Append a new value as well as the last blockchain value to the blockchain.
 
         Arguments:
@@ -171,6 +175,16 @@ class Blockchain:
         if Verification.verify_transaction(transaction, self.get_balance):
             self.__open_transactions.append(transaction)
             self.save_data()
+            if not is_receiving:
+                for node in self.__peer_nodes:
+                    url = "http://{}/broadcast-transaction".format(node)
+                    try:
+                        response = requests.post(url, json={"sender": sender, "recipient": recipient, "amount": amount, "signature": signature})
+                        if response.status_code == 400 or response.status_code == 500:
+                            print("Transaction was declined. This needs resolving")
+                            return False
+                    except requests.exceptions.ConnectionError:
+                        continue
             return True
         return False
 
--- node.py
+++ node.py
@@ -69,6 +69,38 @@ def get_balance():
         return jsonify(response), 500
 
 
+@app.route("/broadcast-transaction")
+def broadcast_transaction():
+    values = request.get_json()
+    if not values:
+        response = {"message": "No data found!"}
+        return jsonify(response), 400
+    required = ["sender", "recipient", "amount", "signature"]
+    if not all(key in values for key in required):
+        response = {"message": "Some data is missing!"}
+        return jsonify(response), 400
+    success = blockchain.add_transaction(
+        values["recipient"], values["sender"], values["amount"], values["signature"], is_receiving=True)
+    if success:
+        response = {
+            "message": "Transaction added successfully",
+            "tranaction": {
+                "sender": values["sender"],
+                "recipient": values["recipient"],
+                "amount": values["amount"],
+                "signature": values["signature"]
+            }
+        }
+        return jsonify(response), 201
+
+    else:
+        response = {
+            "message": "Creating a transaction failed!"
+        }
+        return jsonify(response), 500
+
+
+
 @app.route("/transaction", methods=["POST"])
 def add_transaction():
     if wallet.public_key == None:
