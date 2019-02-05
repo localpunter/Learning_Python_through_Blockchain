7f22cc6be78c79b0f9f552a75d49ccf1774696f4
localpunter <localpunter@sky.com>
2019-02-05 11:48:51 +0000

    Finding and solving conflicts

--- blockchain.py
+++ blockchain.py
@@ -36,7 +36,9 @@ class Blockchain:
         self.public_key = public_key
         self.__peer_nodes = set()
         self.node_id = node_id
+        self.resolve_conflicts = False
         self.load_data()
+
 
     # This turns the chain attribute into a property with a getter (the method below) and a setter (@chain.setter)
     @property
@@ -169,8 +171,8 @@ class Blockchain:
         #     'recipient': recipient,
         #     'amount': amount
         # }
-        if self.public_key == None:
-            return False
+        # if self.public_key == None:
+        #     return False
         transaction = Transaction(sender, recipient, signature, amount)
         if Verification.verify_transaction(transaction, self.get_balance):
             self.__open_transactions.append(transaction)
@@ -181,7 +183,7 @@ class Blockchain:
                     try:
                         response = requests.post(url, json={"sender": sender, "recipient": recipient, "amount": amount, "signature": signature})
                         if response.status_code == 400 or response.status_code == 500:
-                            print("Transaction was declined. This needs resolving")
+                            print("Transaction was declined. This needs resolving!")
                             return False
                     except requests.exceptions.ConnectionError:
                         continue
@@ -216,7 +218,41 @@ class Blockchain:
         self.__chain.append(block)
         self.__open_transactions = []
         self.save_data()
+        for node in self.__peer_nodes:
+            url = "http://{}/broadcast-block".format(node)
+            converted_block = block.__dict__.copy()
+            converted_block["transactions"] = [tx.__dict__ for tx in converted_block["transactions"]]
+            try:
+                response = requests.post(url, json={"block": converted_block})
+                if response.status_code == 400 or response.status_code == 500:
+                    print("Block was declined. This needs resolving!")
+                if response.status_code == 409:
+                    self.resolve_conflicts = True
+            except requests.exceptions.ConnectionError:
+                continue
         return block
+
+
+    def add_block(self, block):
+        transactions = [Transaction(
+            tx["sender"], tx["recipient"], tx["signature"], tx["amount"]) for tx in block["transactions"]]
+        proof_is_valid = Verification.valid_proof(
+            transactions[:-1], block["previous_hash"], block["proof"])
+        hashes_match = hash_block(self.chain[-1]) == block["previous_hash"]
+        if not proof_is_valid or not hashes_match:
+            return False
+        converted_block = Block(block["index"], block["previous_hash"], transactions, block["proof"], block["timestamp"])
+        self.__chain.append(converted_block)
+        stored_transactions = self.__open_transactions[:]
+        for itx in block["transactions"]:
+            for opentx in stored_transactions:
+                if opentx.sender == itx["sender"] and opentx.recipient == itx["recipient"] and opentx.amount == itx["amount"] and opentx.signature == itx["signature"]:
+                    try:
+                        self.__open_transactions.remove(opentx)
+                    except ValueError:
+                        print("This item was already removed!")
+        self.save_data()
+        return True
 
 
     def add_peer_node(self, node):
--- node.py
+++ node.py
@@ -100,6 +100,32 @@ def broadcast_transaction():
         return jsonify(response), 500
 
 
+@app.route("/broadcast-block", methods=["POST"])
+def broadcast_block():
+    values = request.get_json()
+    if not values:
+        response = {"message": "No data found!"}
+        return jsonify(response), 400
+    if "block" not in values:
+        response = {"message": "Some data is missing!"}
+        return jsonify(response), 400
+    block = values["block"]
+    if block["index"] == blockchain.chain[-1].index + 1:
+        if blockchain.add_block(block):
+            response = {"message": "Block added!"}
+            return jsonify(response), 201
+        else:
+            response = {"message": "Block is invalid!"}
+            return jsonify(response), 409
+    elif block["index"] > blockchain.chain[-1].index:
+        response = {"message": "This blockchain is different to the local blockchain!"}
+        blockchain.resolve_conflicts = True
+        return jsonify(response), 200
+    else:
+        response = {"message": "Blockchain is shorter than expected! Block not added!"}
+        return jsonify(response), 409
+
+
 
 @app.route("/transaction", methods=["POST"])
 def add_transaction():
@@ -147,6 +173,9 @@ def add_transaction():
 
 @app.route("/mine", methods=["POST"])
 def mine():
+    if blockchain.resolve_conflicts:
+        response = {"message":"Resolve conflicts first! Block not added!"}
+        return jsonify(response), 409
     block = blockchain.mine_block()
     if block != None:
         dict_block = block.__dict__.copy()
